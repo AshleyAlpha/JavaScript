9771e064b74a4adb2fba975f6a6a9af9dcf1de85
AshleyAlpha <mashleyalpha@gmail.com>
2024-02-26 03:08:12 -0800

    my first javascript

---
+++ DAY2/variable.js
@@ -0,0 +1,21 @@
+// going to focus on variables and data 
+// 1. numeric variables
+// srting variables 
+// boolean variables 
+// array variables 
+const age = 62; //number type variable 
+const name= "grand mere de lucinda falls"
+const array=["string", 12, false];
+const myfood=["Umuceri", "amateke", "inyama", "ifiriti"]
+console.log(" i like to eat " + array)
+console.log(" my name is " + name + " and am " + age + " years old ")
+
+function myLastName(){
+    let me = "Mamie";
+    console.log(me);
+}
+myLastName();
+
+let FirstNumber = 5;
+let SecondNumber = 0; 
+console.log(FirstNumber % SecondNumber);
