3e69d226ff73821467e4d0d25ac8f5195e2681d7
AshleyAlpha <mashleyalpha@gmail.com>
2024-02-29 02:09:49 -0800

    added a new file called

---
+++ DAY4/math.js
@@ -0,0 +1,38 @@
+num = -4;
+console.log(Math.abs (num)); //it returns a positive number when you have assigned it a negative number
+
+num = 4;
+console.log(Math.abs (num));
+
+number = 3.5;
+console.log(Math.ceil(number));
+
+number = 3.5;
+console.log(Math.floor(number));
+
+number = 3.5;
+console.log(Math.round(number));
+
+number = 3.5;
+number2 =4;
+number3 = 10;
+
+console.log(Math.max(number, number2, number3));
+
+number = 3.5;
+number2 =4;
+number3 = 10;
+
+console.log(Math.min(number, number2, number3));
+
+number3 = 10;
+console.log(Math.pow(number3, 2));
+
+number3 = 25;
+console.log(Math.sqrt(number3));
+
+
+    
+
+
+
